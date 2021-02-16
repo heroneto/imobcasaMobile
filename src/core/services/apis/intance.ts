3149b7a39351541ed2a1c3565e2a2ce7ebb08b55
Heron Eto <41599309+heroneto@users.noreply.github.com>
2021-02-16 20:06:51 -0300

    Add new endpoint

--- src/core/services/apis/intance.ts
+++ src/core/services/apis/intance.ts
@@ -1,6 +1,9 @@
 import axios from 'axios'
+
+
+const HOST = __DEV__ ? "http://192.168.0.2:8000" : "https://imobcasa.herokuapp.com"
 const instance = axios.create({
-  baseURL: "http://192.168.14.112:8000"
+  baseURL: HOST
 })
 
 export default instance
